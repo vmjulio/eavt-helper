132700b8fe15b890e36553354bb445b8cb7d3611
vmjulio <vijulio@gmail.com>
2022-01-10 16:20:50 -0300

    fix module name

--- eavt_helper/commands/__init__.py
+++ eavt_helper/commands/__init__.py
@@ -1,5 +1,5 @@
 """
 Exports for CLI commands.
 """
-from eavt_handler.commands.eavt_to_scd import eavt_to_scd
-from eavt_handler.commands.snapshot_to_eavt import snapshot_to_eavt
+from eavt_helper.commands.eavt_to_scd import eavt_to_scd
+from eavt_helper.commands.snapshot_to_eavt import snapshot_to_eavt
