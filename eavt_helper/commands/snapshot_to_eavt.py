57bc9a25b0e9c369f79a1c58c4dfb6cb918e384e
vmjulio <vijulio@gmail.com>
2022-01-10 23:35:23 -0300

    break into smaller methods

--- eavt_helper/commands/snapshot_to_eavt.py
+++ eavt_helper/commands/snapshot_to_eavt.py
@@ -13,13 +13,33 @@ import click
 @click.argument("out_path")
 def snapshot_to_eavt(snapshot_path, id_col="id", tstamp_col="run", out_path="out.csv"):
     snapshot_df = read_dataframe_from_csv(snapshot_path)
-    df = snapshot_df.set_index([id_col, tstamp_col])
-    df = df.stack().reset_index()
-    df.rename(columns={'id': 'e', 'level_2':
-                       'a', 0: 'v', 'run': 't'}, inplace=True)
-    df = df[['e', 'a', 'v', 't']]
-    df['v_lag'] = df.sort_values(by=['t'], ascending=True)\
-                    .groupby(['e', 'a'])['v'].shift(1)
-    df = df[df["v"] != df["v_lag"]]
-    df = df.drop(columns=["v_lag"])
+    df = (snapshot_df.pipe(with_set_index, [id_col, tstamp_col])
+                     .pipe(with_stack_reset_index)
+                     .pipe(with_rename_cols_eavt)
+                     .pipe(with_order_eavt)
+                     .pipe(with_v_lag)
+                     .pipe(with_different_v_lag)
+                     .pipe(with_drop_v_lag))
     write_dataframe_to_csv(df, out_path)
+
+def with_set_index(df, list_index):
+    return df.set_index(list_index)
+
+def with_stack_reset_index(df):
+    return df.stack().reset_index()
+
+def with_rename_cols_eavt(df):
+    return df.rename(columns={'id': 'e', 'level_2': 'a', 0: 'v', 'run': 't'})
+
+def with_order_eavt(df):
+    return df[['e', 'a', 'v', 't']]
+
+def with_v_lag(df):
+    df["v_lag"] = df.sort_values(by=['t'], ascending=True).groupby(['e', 'a'])['v'].shift(1)
+    return df
+
+def with_different_v_lag(df):
+    return df[df["v"] != df["v_lag"]]
+
+def with_drop_v_lag(df):
+    return df.drop(columns=["v_lag"])
