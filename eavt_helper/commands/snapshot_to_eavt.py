dbef5c31f49f1ea4912f01db70ae56f1ad59a1f0
Vitor <vijulio@gmail.com>
2024-06-21 17:07:14 +0200

    fix command

--- eavt_helper/commands/snapshot_to_eavt.py
+++ eavt_helper/commands/snapshot_to_eavt.py
@@ -14,22 +14,32 @@ import click
 def snapshot_to_eavt(snapshot_path, id_col="id", tstamp_col="run", out_path="out.csv"):
     snapshot_df = read_dataframe_from_csv(snapshot_path)
     df = (snapshot_df.pipe(with_set_index, [id_col, tstamp_col])
+                     .pipe(with_drop_snapshot_columns)
                      .pipe(with_stack_reset_index)
-                     .pipe(with_rename_cols_eavt, id_col)
+                     .pipe(with_rename_cols_eavt, id_col, tstamp_col)
                      .pipe(with_order_eavt)
                      .pipe(with_v_lag)
                      .pipe(with_different_v_lag)
                      .pipe(with_drop_v_lag))
     write_dataframe_to_csv(df, out_path)
 
+def with_drop_snapshot_columns(df):
+    if 'row_version' in df.columns:
+        df = df.drop(columns=["row_version"])
+    if 'current_row_indicator' in df.columns:
+        df = df.drop(columns=["current_row_indicator"])
+    if 'row_expiration_tstamp' in df.columns:
+        df = df.drop(columns=["row_expiration_tstamp"])
+    return df
+    
 def with_set_index(df, list_index):
     return df.set_index(list_index)
 
 def with_stack_reset_index(df):
     return df.stack().reset_index()
 
-def with_rename_cols_eavt(df, id_col):
-    return df.rename(columns={id_col: 'e', 'level_2': 'a', 0: 'v', 'run': 't'})
+def with_rename_cols_eavt(df, id_col, tstamp_col):
+    return df.rename(columns={id_col: 'e', 'level_2': 'a', 0: 'v', tstamp_col: 't'})
 
 def with_order_eavt(df):
     return df[['e', 'a', 'v', 't']]
