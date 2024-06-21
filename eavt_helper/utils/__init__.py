0a25ade0a97f52b0184577603cadf9f709ac783d
Vitor <vijulio@gmail.com>
2024-06-21 15:43:15 +0200

    fix typo

--- eavt_helper/utils/__init__.py
+++ eavt_helper/utils/__init__.py
@@ -5,5 +5,5 @@ def read_dataframe_from_csv(path: str) -> pd.DataFrame:
     return pd.read_csv(path)
 
 
-def write_dataframe_to_csv(df, path) -> None:
-    df.to_csv(path, index=False):
+def write_dataframe_to_csv(df: pd.DataFrame, path: str) -> None:
+    df.to_csv(path, index=False)
